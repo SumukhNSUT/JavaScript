4baf9fea02646506eba02dbb2f272fafe209c925
Sumukh Bansal <142471848+SumukhNSUT@users.noreply.github.com>
2024-06-12 18:55:39 +0530

    commit

---
+++ first.js
@@ -0,0 +1,11 @@
+console.log("hello");
+fullName = "Sumukh Bansal";
+console.log(fullName);
+{
+	let a = 10;
+	console.log(a);
+
+	let b = 5;
+}
+// console.log(b);
+// give error as b was defined in block
