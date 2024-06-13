f787ba0e6d510c201e8de44d2b7b2c3448e69ee6
Sumukh Bansal <142471848+SumukhNSUT@users.noreply.github.com>
2024-06-13 22:53:09 +0530

    prompt and alert

---
+++ second/second.js
@@ -0,0 +1,10 @@
+// prompt gives a pop-up and takes input
+let age = prompt("Enter your age");
+if (age >= 18) {
+	console.log("You can vote!");
+} else {
+	console.log("You cannot vote!");
+}
+
+// alert gives a pop-up but dont take input
+alert("this is alert!");
