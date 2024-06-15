9cc6ce973c509c0f5510f9cd7bd5487d5e7f4066
Sumukh Bansal <142471848+SumukhNSUT@users.noreply.github.com>
2024-06-15 11:48:38 +0530

    functions js

---
+++ Functions_js/script.js
@@ -0,0 +1,23 @@
+function myFun() {
+	console.log("Function is called");
+}
+
+function myFunction(x) {
+	console.log(x * 2);
+}
+
+function sum(x, y) {
+	let result = x + y;
+	return result; // or return x + y;
+	console.log("won't execute because after return");
+}
+// using arrow operator
+// compact way of writting functions
+const printHello = () => {
+	console.log("Hello world!");
+};
+
+myFun();
+myFunction(2);
+let ans = sum(3, 4);
+console.log(ans);
